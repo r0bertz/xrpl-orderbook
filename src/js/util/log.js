2d67bc0ac9953bdcb082db761a7b351546cbbf3f
Le Zhang <robert.zhangle@gmail.com>
2019-03-10 16:31:23 -0700

    Delete src/js/util/log.js

--- src/js/util/log.js
+++
@@ -1,9 +0,0 @@
-/**
- * Print an exception for debug purposes.
- *
- * Includes some logic to try and log a stack in various browsers.
- */
-exports.exception = function (exception) {
-  console.log("function" === typeof exception.getStack ? exception.getStack() : exception.stack);
-};
-
