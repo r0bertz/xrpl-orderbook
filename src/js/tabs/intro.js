7f53b14221ca407f99264c340c4d56a4a6f46307
Le Zhang <robert.zhangle@gmail.com>
2019-03-15 21:43:58 -0700

    Make it easier to enter currency pair
    Now user only need to copy and paste an xrpcharts link.
    
    Also put source cod on github. Added link to my blog, validator and xrptipbot account.

--- src/js/tabs/intro.js
+++ src/js/tabs/intro.js
@@ -22,19 +22,26 @@ IntroTab.prototype.angular = function(module)
   module.controller('IntroCtrl', ['$scope', '$rootScope', '$window',
     function($scope, $rootScope, $window) {
       $scope.Options = $rootScope.globalOptions;
-      $scope.pair = 'XRP/XLM:rKiCet8SdvWxPXnAgYarFUXMh1zCPz432Y';
-      $scope.account = 'rLLgJmQxrLEXrSDNkQEr9tvGoUdafFHTKP';
-      $scope.showAccount = true;
-      $scope.clickShowAccount = function(showAccount) {
-        if (!showAccount) {
+      $scope.link = 'https://xrpcharts.ripple.com/#/markets/XRP/CNY:rKiCet8SdvWxPXnAgYarFUXMh1zCPz432Y?interval=5m&range=1d&type=candlestick'
+      $scope.account = 'rJMWjmyAuJxG96SwU9iASCPz9GrpTivtzB';
+      $scope.highlightAccount = true;
+      $scope.clickHighlightAccount = function(highlightAccount) {
+        if (!highlightAccount) {
           $scope.account = undefined;
         }
       }
+      var linkPattern = /https:\/\/xrpcharts.ripple.com\/#\/markets\/([^\/]+\/[^?]+).*/g
       $scope.showOrderbook = function() {
+        var match = linkPattern.exec($scope.link)
+        if (!match) {
+          alert('Invalid link: ' + $scope.link)
+          return
+        }
+        var pair = match[1]
         if ($scope.account) {
-          $window.location.href = '/#/orderbook/' + $scope.pair + '/' + $scope.account;
+          $window.location.href = '/#/orderbook/' + pair + '/' + $scope.account;
         } else {
-          $window.location.href = '/#/orderbook/' + $scope.pair;
+          $window.location.href = '/#/orderbook/' + pair;
         }
       };
     }
